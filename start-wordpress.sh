2e337f00a05ac4d16de524f1df861d68248844b0
Florian Assmus <fassmus@Florians-MacBook-Pro.local>
2020-10-30 17:42:59 +0100

    Improve script

--- start-wordpress.sh
+++ start-wordpress.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+PUB_IP="$( dig @ns1-1.akamaitech.net ANY whoami.akamai.net +short )"
+
 sudo apt-get update
 sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
 
@@ -12,7 +14,7 @@ sudo add-apt-repository \
 # Install Docker
 sudo apt-get -y install docker-ce
 
-echo -n "Enter DB URL [ENTER]: "
+echo -n "\n\nEnter DB URL [ENTER]: "
 read db_url
 
 echo -n "Enter DB username [ENTER]: "
@@ -21,9 +23,11 @@ read db_user
 echo -n "Enter DB password [ENTER]: "
 read -s db_pass
 
-echo "Creating database 'wordpress' on MySQL server..."
+echo "\n\nCreating database 'wordpress' on MySQL server..."
 echo "create database wordpress" | mysql --host=$db_url --user=$db_user --password=$db_pass
 echo "Database created!"
 
 echo "Starting Wordpress server.."
 sudo docker run -d -e WORDPRESS_DB_HOST=$db_url -e WORDPRESS_DB_USER=$db_user -e WORDPRESS_DB_PASSWORD=$db_pass -e WORDPRESS_DB_NAME=wordpress -e WORDPRESS_TABLE_PREFIX=wp_ --network host wordpress
+
+echo "Access Wordpress via http://$PUB_IP"
