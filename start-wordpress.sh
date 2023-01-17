c43ad0332199ffd3978aed11e189346d080f6634
Markus Walker <markus.walker@prodyna.com>
2023-01-17 08:51:28 +0100

    Update Dependencies for Ubuntu 22.04 + add yes flag so no interaction is required

--- start-wordpress.sh
+++ start-wordpress.sh
@@ -6,13 +6,13 @@ sudo apt-get -y install apt-transport-https ca-certificates curl software-proper
 
 # Add apt repos
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
+sudo --yes add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
 # Install Docker
-sudo apt-get -y install docker-ce mysql-client-core-5.7
+sudo apt-get -y install docker-ce mariadb-client-core-10.6
 
 echo  -e -n "\n\nEnter DB URL [ENTER]: "
 read db_url
